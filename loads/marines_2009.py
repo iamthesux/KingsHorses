4f0ecf3167b55e71ad355cac2f91622d476d620a
ClineTso <tjcjttt@hotmail.com>
2015-02-04 01:16:52 -0700

    fixed and added loadouts to marines

--- loads/marines_2009.py
+++ loads/marines_2009.py
@@ -4,12 +4,12 @@ class marine_base(Base):
 	headgear = 'H_VTN_LWH_WDL'
 	
 	items = Base.items + [
-		'rhsusf_ANPVS_15',
+		'rhsusf_ANPVS_14',
 		'tf_rf7800str',
 	]
 	class HandGun:
-		weapon = 'CUP_hgun_M9'
-		mags = [['CUP_15Rnd_9x19_M9', 15]]
+		weapon = 'RH_m9'
+		mags = [['RH_15Rnd_9x19_M9', 15]]
 
 	class Uniform:
 		type = 'VTN_U_FROG_WDL'
@@ -31,19 +31,19 @@ class marine_base(Base):
 
 class marine_rifleman(marine_base):
 	class Primary:
-		weapon = 'rhs_weap_m16a4_carryhandle'
+		weapon = 'RH_M16A4'
 		optic = 'RH_ta31rco_2D'
-		rail = 'rhsusf_acc_ACOG3'
+		rail = 'RH_peq15'
 		mags = [
-			['rhs_mag_30Rnd_556x45_M855A1_Stanag', 30],
+			['RH_30Rnd_556x45_M855A1', 30],
 		]
 	class Vest(marine_base.Vest):
 		items = marine_base.Vest.items + [
-			['rhs_mag_30Rnd_556x45_M855A1_Stanag', 5],
+			['RH_30Rnd_556x45_M855A1', 5],
 		]
 	class Backpack(marine_base.Backpack):
 		items = marine_base.Backpack.items + [
-			['rhs_mag_30Rnd_556x45_M855A1_Stanag', 5],
+			['RH_30Rnd_556x45_M855A1', 5],
 		]
 
 
@@ -54,9 +54,9 @@ class marine_tl(marine_rifleman):
 	class Primary:
 		weapon = 'RH_M16A4gl'
 		optic = 'RH_ta31rco_2D'
-		rail = 'rhsusf_acc_anpeq15A'
+		rail = 'RH_peq15'
 		mags = [
-			['rhs_mag_30Rnd_556x45_M855A1_Stanag', 30],
+			['RH_30Rnd_556x45_M855A1', 30],
 			['1Rnd_HE_Grenade_shell', 1],
 		]
 	class Vest(marine_rifleman.Vest):
@@ -74,19 +74,19 @@ class marine_tl(marine_rifleman):
 
 class marine_sl(marine_tl):
 	class Primary:
-		weapon = 'RH_M16A4gl'
+		weapon = 'RH_M4_ris_M203'
 		optic = 'RH_ta31rco_2D'
-		rail = 'rhsusf_acc_anpeq15A'
+		rail = 'RH_peq15'
 		mags = [
-			['rhs_mag_30Rnd_556x45_M855A1_Stanag', 30],
+			['RH_30Rnd_556x45_M855A1', 30],
 			['1Rnd_HE_Grenade_shell', 1],
 		]
 
 class marine_ar(marine_base):
 	class Primary:
 		weapon = 'rhs_weap_m249_pip'
-		optic = 'rhsusf_acc_ACOG2'
-		rail = 'rhsusf_acc_anpeq15A'
+		optic = 'RH_ta31rco_2D'
+		rail = 'RH_peq15'
 		mags = [
 			['rhsusf_100Rnd_556x45_soft_pouch', 100],
 		]
@@ -105,4 +105,50 @@ class marine_aar(marine_rifleman):
 	class Backpack(marine_rifleman.Backpack):
 		items = marine_rifleman.Backpack.items + [
 			['rhsusf_100Rnd_556x45_soft_pouch', 5],
+		]
+
+class marine_mg(marine_base):
+	class Primary:
+		weapon = 'rhs_weap_m240B'
+		optic = 'RH_ta31rco_2D'
+		rail = 'RH_peq15'
+		mags = [
+			['rhsusf_100Rnd_762x51', 100],
+		]
+	class Vest(marine_base.Vest):
+		type = 'V_MTV_MG'
+
+	class Backpack(marine_base.Backpack):
+		items = marine_base.Backpack.items + [
+			['rhsusf_100Rnd_762x51', 5],
+		]
+
+class marine_amg(marine_rifleman):
+	class Vest(marine_rifleman.Vest):
+		type = 'V_MTV_MG'
+
+	class Backpack(marine_rifleman.Backpack):
+		items = marine_rifleman.Backpack.items + [
+			['rhsusf_100Rnd_762x51', 5],
+		]
+
+class marine_at(marine_rifleman):
+	class Secondary:
+		weapon = 'CUP_launch_Mk153Mod0'
+		mags = [
+			['CUP_SMAW_HEAA_M', 1],
+		]
+
+	class Backpack(marine_rifleman.Backpack):
+		items = marine_rifleman.Backpack.items + [
+			['CUP_SMAW_HEAA_M', 1],
+			['CUP_SMAW_HEDP_M', 1],
+			['CUP_optic_SMAW_Scope', 1],
+		]
+
+class marine_aat(marine_rifleman):
+	class Backpack(marine_rifleman.Backpack):
+		items = marine_rifleman.Backpack.items + [
+			['CUP_SMAW_HEDP_M', 1],
+			['CUP_SMAW_HEAA_M', 1],
 		]
