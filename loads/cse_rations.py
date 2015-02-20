e1080cd59b471ad3032c8d054b1919b82778f091
LewisSO <daniel_ben_lewis@yahoo.com.au>
2015-02-20 12:24:02 +1000

    Added CSE Rations Crates
    US Crate and International Crate

---
+++ loads/cse_rations.py
@@ -0,0 +1,17 @@
+from p4a.loadout import Crate
+
+class int_supplies(Crate):
+	base = 'VTN_WPNW_FN_M_BOX'
+	title = 'International Rations'
+	items = [
+		['cse_canteen', 500],
+		['cse_MRE_LambC', 200],
+		['cse_MRE_Rice', 200],
+		['cse_MRE_CreamTomatoSoup', 200],
+		['cse_MRE_CreamChickenSoup', 200],
+		['cse_MRE_ChickenTikkaMassala', 200],
+		['cse_MRE_SteakVegetables', 200],
+		['cse_MRE_MeatballsPasta', 200],
+		['cse_MRE_ChickenHerbDumplings', 200],
+		['cse_Humanitarian_Ration', 200],
+	]
---
+++ loads/cse_usrations.py
@@ -0,0 +1,33 @@
+from p4a.loadout import Crate
+
+class us_supplies(Crate):
+	base = 'VTN_WPNW_FN_M_BOX'
+	title = 'US Rations'
+	items = [
+		['cse_waterbottle', 500],
+		['cse_US_MRE_ChiliBeans', 100],
+		['cse_US_MRE_ChickenFajita', 100],
+		['cse_US_MRE_ChickenNoodles', 100],
+		['cse_US_MRE_PorkSausageGravy', 100],
+		['cse_US_MRE_MedChicen', 100],
+		['cse_US_MRE_BeefRoastVeggies', 100],
+		['cse_US_MRE_BeefBrisket', 100],
+		['cse_US_MRE_MeatballMarinara', 100],
+		['cse_US_MRE_BeefStew', 100],
+		['cse_US_MRE_ChiliMacaroni', 100],
+		['cse_US_MRE_VegetableLasagna', 100],
+		['cse_US_MRE_SpicyPennePasta', 100],
+		['cse_US_MRE_CheeseTortellini', 100],
+		['cse_US_MRE_Ratatouille', 100],
+		['cse_US_MRE_MexicanStyleChickenStew', 100],
+		['cse_US_MRE_PorkRib', 100],
+		['cse_US_MRE_MapleSausage', 100],
+		['cse_US_MRE_BeefRavioli', 100],
+		['cse_US_MRE_SloppyJoe', 100],
+		['cse_US_MRE_SpaghettiMeatSauce', 100],
+		['cse_US_MRE_LemonPepperTuna', 100],
+		['cse_US_MRE_AsianBeefStrips', 100],
+		['cse_US_MRE_ChickenPestoPasta', 100],
+		['cse_US_MRE_SouthwestStyleBeefBlackBeans', 100],
+		['cse_Humanitarian_Ration', 100],
+	]
